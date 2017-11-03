8ee4959b5e0571c639313b5e1b023bdadb467de3
Jaeyoung Lee <jylee0121@gmail.com>
2017-11-03 13:33:19 +0900

    change port setup for NoEditor mode
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- Source/UnrealCV/Private/UnrealCVModule.cpp
+++ Source/UnrealCV/Private/UnrealCVModule.cpp
@@ -12,12 +12,26 @@ IMPLEMENT_MODULE(FUnrealCVPlugin, UnrealCV)
 
 void FUnrealCVPlugin::StartupModule()
 {
-	//FString DeviceProfileName = UDeviceProfileManager::Get().GetActiveProfileName();
+	FString DeviceProfileName = UDeviceProfileManager::Get().GetActiveProfileName();
 	UDeviceProfile *DeviceProfile = UDeviceProfileManager::Get().GetActiveProfile();
-	FString DeviceProfileName = DeviceProfile->BaseProfileName;
+	//FString DeviceProfileName = DeviceProfile->BaseProfileName;
+	UE_LOG(LogUnrealCV, Warning, TEXT("====================================================================="));
 	UE_LOG(LogUnrealCV, Warning, TEXT("DeviceProfileName = %s"), *DeviceProfileName);
+	UE_LOG(LogUnrealCV, Warning, TEXT("DeviceProfile->BaseProfileName = %s"), *DeviceProfile->BaseProfileName);
+	UE_LOG(LogUnrealCV, Warning, TEXT("DeviceProfile->DeviceType = %s"), *DeviceProfile->DeviceType);
+	UE_LOG(LogUnrealCV, Warning, TEXT("DeviceProfile->ConfigPlatform = %s"), *DeviceProfile->ConfigPlatform);
+	for (auto elem : DeviceProfile->CVars)
+	{
+		UE_LOG(LogUnrealCV, Warning, TEXT("DeviceProfile->ConfigPlatform = %s %s"), *elem);
+	}
+	UE_LOG(LogUnrealCV, Warning, TEXT("====================================================================="));
+
 	FUE4CVServer &Server = FUE4CVServer::Get();
 	Server.RegisterCommandHandlers();
+
+	if (DeviceProfileName.Equals(TEXT("WindowsNoEditor")))
+		Server.Config.Port += 1;
+
 	Server.NetworkManager->Start(Server.Config.Port);
 }
 
