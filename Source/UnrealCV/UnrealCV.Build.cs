6c085cd1d796b0ef8c8e195d61d497775ee6f193
Jaeyoung Lee <jylee0121@gmail.com>
2018-02-21 17:00:03 +0900

    update setup for UROSBridge
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- Source/UnrealCV/UnrealCV.Build.cs
+++ Source/UnrealCV/UnrealCV.Build.cs
@@ -1,62 +1,69 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+using UnrealBuildTool;
 using System.IO;
 
-namespace UnrealBuildTool.Rules
+public class UnrealCV: ModuleRules
 {
-	public class UnrealCV: ModuleRules
-	{
-		public UnrealCV(TargetInfo Target)
-		{
-			// This trick is from https://answers.unrealengine.com/questions/258689/how-to-include-private-header-files-of-other-modul.html
-			string EnginePath = Path.GetFullPath(BuildConfiguration.RelativeEnginePath);
+    public UnrealCV(ReadOnlyTargetRules Target) : base(Target)
+    {
+		// This trick is from https://answers.unrealengine.com/questions/258689/how-to-include-private-header-files-of-other-modul.html
+		string EnginePath = Path.GetFullPath(BuildConfiguration.RelativeEnginePath);
 
-			PublicIncludePaths.AddRange(
-				new string[]
-				{
-					EnginePath + "Source/Runtime/Launch/Resources",
-					// To get Unreal Engine minor version
-				}
-			);
-
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"UnrealCV/Private/Commands",
-					"UnrealCV/Private/libs", // For 3rd-party libs
-				}
-			);
-
-			PublicDependencyModuleNames.AddRange(new string[] {
-				"Core",
-				"CoreUObject",
-				"Engine",
-				"InputCore",
-				"RenderCore",
-				"Networking",
-				"Sockets",
-				"Slate",
-				"ImageWrapper",
-				"CinematicCamera",
-				"Projects", // Support IPluginManager
-			});
-
-			// PrivateDependency only available in Private folder
-			// Reference: https://answers.unrealengine.com/questions/23384/what-is-the-difference-between-publicdependencymod.html
-			if (UEBuildConfiguration.bBuildEditor == true)
+		PublicIncludePaths.AddRange(
+			new string[]
 			{
-				PrivateDependencyModuleNames.AddRange(
-					new string[]
-					{
-						"UnrealEd", // To support GetGameWorld
-					}
-				);
+				EnginePath + "Source/Runtime/Launch/Resources",
+				// To get Unreal Engine minor version
 			}
+		);
 
-			DynamicallyLoadedModuleNames.AddRange(
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"UnrealCV/Private/Commands",
+				"UnrealCV/Private/libs", // For 3rd-party libs
+			}
+		);
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"RenderCore",
+			"Networking",
+			"Sockets",
+			"Slate",
+			"ImageWrapper",
+			"CinematicCamera",
+			"Projects", // Support IPluginManager
+            "UROSBridge",
+            "Json",
+            "JsonUtilities",
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            //"UROSBridge",
+            // ... add private dependencies that you statically link with here ...
+        });
+
+        // PrivateDependency only available in Private folder
+        // Reference: https://answers.unrealengine.com/questions/23384/what-is-the-difference-between-publicdependencymod.html
+        if (UEBuildConfiguration.bBuildEditor == true)
+		{
+			PrivateDependencyModuleNames.AddRange(
 				new string[]
 				{
-					"Renderer"
+					"UnrealEd", // To support GetGameWorld
 				}
 			);
 		}
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				"Renderer"
+			}
+		);
 	}
 }
+
