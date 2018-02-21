b88a2bce3e72b1b58e82fcdc2b6dc2ee1da86f67
Jaeyoung Lee <jylee0121@gmail.com>
2018-02-21 17:00:48 +0900

    implement main robot movement
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- Source/UnrealCV/Private/RemoteMovementComponent.cpp
+++ Source/UnrealCV/Private/RemoteMovementComponent.cpp
@@ -1,10 +1,49 @@
 #include "UnrealCVPrivate.h"
 #include "RemoteMovementComponent.h"
+#include "UE4ROSBridgeManager.h"
+#include "rosgraph_msgs/Clock.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "nav_msgs/Odometry.h"
+#include "ROSHelper.h"
+
+URemoteMovementComponent::FROSTwistSubScriber::FROSTwistSubScriber(const FString& InTopic,  URemoteMovementComponent* Component) :
+	FROSBridgeSubscriber(InTopic, TEXT("geometry_msgs/Twist"))
+{
+	this->Component = Component;
+}
+
+URemoteMovementComponent::FROSTwistSubScriber::~FROSTwistSubScriber()
+{
+};
+
+TSharedPtr<FROSBridgeMsg> URemoteMovementComponent::FROSTwistSubScriber::ParseMessage
+(TSharedPtr<FJsonObject> JsonObject) const
+{
+	TSharedPtr<geometry_msgs::Twist> TwistMessage = MakeShareable<geometry_msgs::Twist>(new geometry_msgs::Twist());
+	TwistMessage->FromJson(JsonObject);
+	return StaticCastSharedPtr<FROSBridgeMsg>(TwistMessage);
+}
+
+void URemoteMovementComponent::FROSTwistSubScriber::Callback(TSharedPtr<FROSBridgeMsg> InMsg)
+{
+	// downcast to subclass using StaticCastSharedPtr function
+	TSharedPtr<geometry_msgs::Twist> TwistMessage = StaticCastSharedPtr<geometry_msgs::Twist>(InMsg);
+
+	// do something with the message
+	FVector	 Linear = FROSHelper::ConvertVectorROSToUE4(TwistMessage->GetLinear());
+	FRotator Angular = FROSHelper::ConvertEulerAngleROSToUE4(TwistMessage->GetAngular());
+	FTransform Vel(Angular, Linear);
+	this->Component->SetVelocityCmd(Vel);
+	UE_LOG(LogUnrealCV, Log, TEXT("Message received! Content: %s"), *TwistMessage->ToString());
+
+	return;
+}
 
 URemoteMovementComponent::URemoteMovementComponent()
 	:bIsTicking(false)
 {
-
+	this->PrevLinear = FVector(0.0, 0.0, 0.0);
+	this->PrevAngular = FRotator(0.0, 0.0, 0.0);
 }
 
 URemoteMovementComponent* URemoteMovementComponent::Create(FName Name, APawn* Pawn)
@@ -12,6 +51,7 @@ URemoteMovementComponent* URemoteMovementComponent::Create(FName Name, APawn* Pa
 	//URemoteMovementComponent* remoteActor = NewObject<URemoteMovementComponent>((UObject *)GetTransientPackage(), Name);
 	//URemoteMovementComponent* remoteActor = Pawn->CreateDefaultSubobject<URemoteMovementComponent>(Name);
 	URemoteMovementComponent* remoteActor = NewObject<URemoteMovementComponent>((UObject *)Pawn, Name);	
+	remoteActor->AddToRoot();
 	remoteActor->RegisterComponent();
 
 	remoteActor->Init();
@@ -28,6 +68,66 @@ void URemoteMovementComponent::SetVelocityCmd(const FTransform& InVelocityCmd)
 {
 	this->VelocityCmd = InVelocityCmd;
 	this->MoveAnimCountFromRemote = PARAM_MOVE_ANIM_COUNT;
+}
+
+void URemoteMovementComponent::ProcessUROSBridge(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	// publish clock
+	float GameTime = UGameplayStatics::GetRealTimeSeconds(GetWorld());
+	uint64 GameSeconds = (int)GameTime;
+	uint64 GameUseconds = (GameTime - GameSeconds) * 1000000000;
+	TSharedPtr<rosgraph_msgs::Clock> Clock = MakeShareable
+	(new rosgraph_msgs::Clock(FROSTime(GameSeconds, GameUseconds)));
+	Handler->PublishMsg("/clock", Clock);
+
+	// publish tf and odometry
+	APawn* OwningPawn = Cast<APawn>(this->GetOwner());
+	std_msgs::Header Header(0, FROSTime(), "odom");
+
+	FTransform Transform = OwningPawn->GetActorTransform();
+
+	// publish tf
+	geometry_msgs::Transform transform = FROSHelper::ConvertTransformUE4ToROS(Transform);
+
+	TSharedPtr<geometry_msgs::TransformStamped> odomtrans = MakeShareable(new geometry_msgs::TransformStamped(Header, "base_footprint", transform));
+	Handler->PublishMsg("/tf", odomtrans);
+
+	// publish odometry
+	geometry_msgs::Vector3 vec = transform.GetTranslation();
+	geometry_msgs::Quaternion quat = transform.GetRotation();
+	geometry_msgs::Pose pose(geometry_msgs::Point(vec.GetX(), vec.GetY(), vec.GetZ()), quat);
+	TArray<double> posecov = {
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	};
+	geometry_msgs::PoseWithCovariance	posewithcov(pose, posecov);
+
+	geometry_msgs::Vector3 linear	= FROSHelper::ConvertVectorUE4ToROS((Transform.GetLocation() - PrevLinear)/DeltaTime);
+	FRotator AngularVel = FRotator(	(Transform.Rotator().Pitch	- PrevAngular.Pitch	)/ DeltaTime,
+									(Transform.Rotator().Yaw	- PrevAngular.Yaw	)/ DeltaTime,
+									(Transform.Rotator().Roll	- PrevAngular.Roll	)/ DeltaTime);
+	geometry_msgs::Vector3 angular	= FROSHelper::ConvertEulerAngleUE4ToROS(AngularVel);
+	TArray<double> twistcov = {
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+	};
+	geometry_msgs::TwistWithCovariance	twistwithcov(geometry_msgs::Twist(linear, angular), twistcov);
+
+	TSharedPtr<nav_msgs::Odometry> odometry = MakeShareable(new nav_msgs::Odometry(Header, "base_footprint", posewithcov, twistwithcov));
+	Handler->PublishMsg("/ue4/odom", odometry);
+
+	Handler->Process();
+
+	PrevLinear = Transform.GetLocation();
+	PrevAngular = Transform.Rotator();
 }
 
 void URemoteMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
@@ -55,6 +155,8 @@ void URemoteMovementComponent::TickComponent(float DeltaTime, enum ELevelTick Ti
 
 			this->MoveAnimCountFromRemote--;
 		}
+
+		ProcessUROSBridge(DeltaTime, TickType, ThisTickFunction);
 	}
 }
 
@@ -82,5 +184,34 @@ void URemoteMovementComponent::BeginPlay()
 	UWorld* World = FUE4CVServer::Get().GetGameWorld();
 	bool result = World->Exec(World, TEXT("t.MaxFPS 60"));
 	*/
-	//GEngine->Exec(GetWorld(), TEXT("t.MaxFPS 60"));
+	GEngine->Exec(GetWorld(), TEXT("t.MaxFPS 30"));
+
+	// Set websocket server address to ws://127.0.0.1:9001
+	Handler = MakeShareable<FROSBridgeHandler>(new FROSBridgeHandler(TEXT(ROS_MASTER_ADDR), ROS_MASTER_PORT));
+
+	// Add topic subscribers and publishers
+	// Add service clients and servers
+	// **** Create publishers here ****
+	Publisher = MakeShareable<FROSBridgePublisher>(new FROSBridgePublisher(TEXT("/clock"), TEXT("rosgraph_msgs/Clock")));
+	Handler->AddPublisher(Publisher);
+
+	OdomTfPublisher = MakeShareable<FROSBridgePublisher>(new FROSBridgePublisher(TEXT("/tf"), TEXT("geometry_msgs/TransformStamped")));
+	Handler->AddPublisher(OdomTfPublisher);
+	OdomPublisher = MakeShareable<FROSBridgePublisher>(new FROSBridgePublisher(TEXT("/ue4/odom"), TEXT("nav_msgs/Odometry")));
+	Handler->AddPublisher(OdomPublisher);
+
+	// Add topic subscribers and publishers
+	TSharedPtr<FROSTwistSubScriber> Subscriber = MakeShareable<FROSTwistSubScriber>(new FROSTwistSubScriber(TEXT("/ue4/robot/ctrl"), this));
+	Handler->AddSubscriber(Subscriber);
+
+	// Connect to ROSBridge Websocket server.
+	Handler->Connect();
+}
+
+void URemoteMovementComponent::EndPlay(const EEndPlayReason::Type Reason)
+{
+	// Disconnect the handler before parent ends
+	Handler->Disconnect();
+
+	Super::EndPlay(Reason);
 }
--- Source/UnrealCV/Public/RemoteMovementComponent.h
+++ Source/UnrealCV/Public/RemoteMovementComponent.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "Object.h"
+#include "ROSBRidgeHandler.h"
+#include "ROSBridgePublisher.h"
+#include "ROSBridgeSubscriber.h"
+#include "geometry_msgs/Twist.h"
 #include "RemoteMovementComponent.generated.h"
+
 
 /*
 reference: https://docs.unrealengine.com/latest/INT/Programming/Tutorials/Components/4/index.html
@@ -11,6 +16,16 @@ class UNREALCV_API URemoteMovementComponent : public UMovementComponent
 {
 	GENERATED_BODY()
 
+	class FROSTwistSubScriber : public FROSBridgeSubscriber
+	{
+		URemoteMovementComponent* Component;
+	public:
+		FROSTwistSubScriber(const FString& InTopic, URemoteMovementComponent* Component);
+		~FROSTwistSubScriber() override;
+		TSharedPtr<FROSBridgeMsg> ParseMessage(TSharedPtr<FJsonObject> JsonObject) const override;
+		void Callback(TSharedPtr<FROSBridgeMsg> InMsg) override;
+	};
+
 protected:
 	URemoteMovementComponent();
 	FTransform VelocityCmd;
@@ -20,13 +35,28 @@ protected:
 	bool IsMoveToTarget = false;
 	bool bIsTicking ;
 
+	FVector	PrevLinear;
+	FRotator PrevAngular;
+
+	TSharedPtr<FROSBridgeHandler> Handler;
+	TSharedPtr<FROSBridgePublisher> Publisher;
+	TSharedPtr<FROSBridgePublisher> OdomTfPublisher;
+	TSharedPtr<FROSBridgePublisher> OdomPublisher;
+	TSharedPtr<FROSTwistSubScriber> TwistSubScriber;
+
 public:
 	static URemoteMovementComponent* Create(FName Name, APawn* Pawn);
 
 	void Init(void);
 	void SetVelocityCmd(const FTransform& InVelocityCmd);
+	/*
+	* reference : https://docs.unrealengine.com/latest/INT/Programming/UnrealArchitecture/Actors/Ticking/#componentticking
+	*/
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type Reason) override;
 
+protected:
+	void ProcessUROSBridge(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction);
 };
